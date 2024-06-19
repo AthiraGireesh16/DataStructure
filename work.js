12bcc469a31e2ca53e8f89ae884a8dadaf5de928
AthiraGireesh16 <athiragireesh16@gmail.com>
2024-06-19 16:56:23 +0530

    linked list

--- work.js
+++ work.js
@@ -9,30 +9,35 @@ class LinkedList{
         this.head = null
         this.size = 0
     }
+
     isEmpty(){
-        return this.size===0
+         return this.size===0
     }
+
     getSize(){
         return this.size
     }
+
     prepend(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            
+            this.head = node
+           }
+           else{
+            node.next = this.head
+            this.head = node
+           }
+           this.size++
+    }
+
+
+    append(value){
         const node = new Node(value)
         if(this.isEmpty()){
             this.head = node
         }
         else{
-            node.next = this.head
-            this.head = node
-
-        }
-        this.size++
-
-    }
-    append(value){
-        const node = new Node(value)
-        if(this.isEmpty()){
-            this.head = node
-        }else{
             let prev = this.head
             while(prev.next){
                 prev = prev.next
@@ -41,59 +46,93 @@ class LinkedList{
         }
         this.size++
     }
-    insert(value, index){
+    insert(value,index){
+        
         if(index<0 || index > this.size){
-            console.log("enter a valid index");
+            console.log("Enter a valid index");
         }
         if(index === 0){
             this.prepend(value);
-        }
-        else{
+        }else{
             const node = new Node(value)
             let prev = this.head
-            for(let i=0;i<index-1;i++)
-                {
-                    prev = prev.next
-                }
-                node.next = prev.next
-                prev.next = node
-                this.size++
+            for(let i=0;i<index-1;i++){
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next=node
+            this.size++
+        }
+    }
+    removeFrom(index){
+        if(index<0||index>this.size){
+            console.log("Enter valid index");
+        }
+        let removeNode
+        if(index===0){
+            removeNode = this.head.value
+            this.head = this.head.next
+        }
+        else{
+            let prev = this.head
+            for(let i=0;i<index-1;i++){
+                prev = prev.next
+            }
+            removeNode = prev.next
+            prev.next = removeNode.next
+        }
+        this.size--
+        return removeNode
+    }
+    removeValue(value){
+        if(this.isEmpty()){
+            return null
+        }
+        if(this.head.value===value){
+            this.head = this.head.next
+        }
+        else{
+            let prev = this.head
+            while(prev.next.value!==value&&prev.next){
+                prev = prev.next
+            }
+            if(prev.next){
+                const removeNode = prev.next
+                prev.next = removeNode.next
+                this.size--
+                return value
+            }
+            return null
+
         }
     }
 
-    firstRemove(){
-        let curr = this.head
-        this.head = curr.next
-        curr.next = null
-     }
 
-   
     print(){
         if(this.isEmpty()){
-            console.log("List is Empty");
-        }else{
-            let curr  = this.head
-            let listvalues ='';
+            return null;
+        }
+        else{
+            let listValues  = ' '
+            let curr = this.head
             while(curr){
-                listvalues += `${curr.value} `
-                curr = curr.next;
+                listValues +=`${curr.value} `
+                curr = curr.next
             }
-            console.log(listvalues);
+            console.log(listValues);
         }
     }
+
 }
 
 const list = new LinkedList()
-console.log("List empty",list.isEmpty());
-list.print();
+list.append(80)
 list.prepend(10)
-list.prepend(20)
-list.print();
-list.append(50)
 list.append(70)
+list.prepend(20)
 list.print()
-list.insert(23,3)
-list.print();
-console.log("remove first");
-list.firstRemove();
+list.insert(3,2)
+list.print()
+//list.removeFrom(3)
+list.removeValue(3)
 list.print()
