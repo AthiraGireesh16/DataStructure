bcd702695782a2eb817dbfee85a20102072bc349
AthiraGireesh16 <athiragireesh16@gmail.com>
2024-06-24 18:00:31 +0530

    String Question

--- string.js
+++ string.js
@@ -1,53 +1,55 @@
 
 // Quetion 1:Replace each alphabet with another Alphabet at the n-th position
 
+/*
+function replaceAlphabet(str,n){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let result=''
+    for(let i=0;i<str.length;i++){
+        const char = str[i].toLowerCase();
+        const index = alphabet.indexOf(char);
+        if(index!== -1){
+            result += alphabet[(index+n)%26]
+        }else{
+            result += str[i]
+        }
+    }
+    return result;
+}
 
-// function replaceAlphabet(str,n){
-//     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-//     let result=''
-//     for(let i=0;i<str.length;i++){
-//         const char = str[i].toLowerCase();
-//         const index = alphabet.indexOf(char);
-//         if(index!== -1){
-//             result += alphabet[(index+n)%26]
-//         }else{
-//             result += str[i]
-//         }
-//     }
-//     return result;
-// }
 
-
-// // const input = 'Athira'
-// // const shift = 2
-// console.log(replaceAlphabet('Hello world!',5));
-
+// const input = 'Athira'
+// const shift = 2
+console.log(replaceAlphabet('Hello world!',5));
+*/
+//=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>
 
 //Question 2 : Reverse Each word in a String
+/*
+function reverse(str){
+    return str.split(' ').map(word=>word.split('').reverse().join('')).join(' ')
+}
 
-// function reverse(str){
-//     return str.split(' ').map(word=>word.split('').reverse().join('')).join(' ')
-// }
+console.log(reverse('Hello World'));
+*/
 
-// console.log(reverse('Hello World'));
 
+
+//=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>
 
 //Question 3: Count the occurence of Each Character
-
-// function charCount(str){
-//     let string = str.toLowerCase()
-//     let count={};
-//     for(let char of string){
-//         if(count[char]){
-//             count[char]++
-//         }else{
-//             count[char]=1;
-//         }
-//     }
-//     return count;
-// }
-
-
-
-// console.log(charCount('HelloHi'));
-
+/*
+function charCount(str){
+    let string = str.toLowerCase()
+    let count={};
+    for(let char of string){
+        if(count[char]){
+            count[char]++
+        }else{
+            count[char]=1;
+        }
+    }
+    return count;
+}
+console.log(charCount('HelloHi'));
+*/
